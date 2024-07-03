2994dc039adc4ef09ee51bdf0bd0d9589fc3549f
Matheuszmt <matheusmpvphg@gmail.com>
2024-07-03 14:57:28 -0300

    Finalizacao do projeto/desafio

--- Desafio.js
+++ Desafio.js
@@ -1,1 +1,38 @@
-console.log("Olá Mundão")
+let nome = "Matheus"
+let xp = 10002
+let nivel
+
+switch(true){
+    case (xp <= 1000):
+    nivel = "Ferro";
+    console.log("O herói de nome",nome,"está no nivel",nivel,)
+    break;
+    case (xp <= 2000):
+    nivel = "Bronze";
+    console.log("O herói de nome",nome,"está no nivel",nivel,)
+    break;
+    case ( xp <= 5000):
+    nivel = "Prata";
+    console.log("O herói de nome",nome,"está no nivel",nivel,)
+    break;
+    case (xp <= 7000):
+    nivel = "Ouro"
+    console.log("O herói de nome",nome,"está no nivel",nivel,)
+    break;
+    case (xp <= 8000):
+    nivel = "Platina"
+    console.log("O herói de nome",nome,"está no nivel",nivel,)
+    break;
+    case (xp <= 9000):
+    nivel = "Ascendente"
+    console.log("O herói de nome",nome,"está no nivel",nivel,)
+    break;
+    case (xp <= 10000):
+    nivel = "Imortal"
+    console.log("O herói de nome",nome,"está no nivel",nivel,)
+    break;
+    case (xp > 10000):
+    nivel = "Radiante"
+    console.log("O herói de nome",nome,"está no nivel",nivel,)
+    break;
+}
