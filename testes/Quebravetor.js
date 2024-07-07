a1c0282373eadb280a6265a515533a297cfeb0f7
Matheuszmt <matheusmpvphg@gmail.com>
2024-07-07 18:46:26 -0300

    Commit 3

--- testes/Quebravetor.js
+++
@@ -1,8 +0,0 @@
-let userName = getFirstName("Matheus Matias-Ferreira-Martins")
-console.log("Seja bem vindo",userName)
-
-
-function getFirstName(name){
-    let firstName = name.split("-")[0]
-    return firstName
-}
